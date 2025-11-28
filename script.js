536ab2aae1d38a5367b4dce0afed33b955628363
abhay bawne <abhaybawne070@gmail.com>
2025-11-29 00:44:13 +0530

    script.js

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const text=p.innerText;
 
 const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+let iteration =0
+
 function randomtext(){
-    const str=text.split("").map(function(){
+    const str=text.split("").map((char,index)=> {
+        if(index<iteration){
+            return char
+        }
         return character.split("") [Math.floor(Math.random()*52)]
     }).join("")
 
     p.innerText=str
+    iteration+=0.2
 }
+setInterval(randomtext,20)
