4de6255092424d6cf49b5ff7c11fdfe15d864e74
abhay bawne <abhaybawne070@gmail.com>
2025-11-28 23:37:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const p=document.querySelector("p");
+const text=p.innerText;
+
+const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+function randomtext(){
+    const str=text.split("").map(function(){
+        return character.split [Math.floor(Math.random()*52)]
+    }).join()
+
+    console.log(str)
+}
