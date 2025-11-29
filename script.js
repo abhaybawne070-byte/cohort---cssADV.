8224f6b97ff1ad15bedaff35e8496ba21eaa1748
abhay bawne <abhaybawne070@gmail.com>
2025-11-29 09:49:13 +0530

    index.html , style.css , script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const text=p.innerText;
 
 const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-let iteration =0
+let iteration =0 
 
 function randomtext(){
     const str=text.split("").map((char,index)=> {
         if(index<iteration){
             return char
         }
-        return character.split("") [Math.floor(Math.random()*character.length)]
+        return character.split("") [Math.floor(Math.random()*character.length )] // character.length ki jagah kitne character hai (52) to vo bhi dal sakte hai
     }).join("")
 
     p.innerText=str
